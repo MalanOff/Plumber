16bb603ac3ba7de4a5ebc834e9abb0a630c8ac68
Malan <710972@gmail.com>
2017-06-13 15:48:28 +0300

    first commit

---
+++ app/js/js.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+      
+    items:1,
+    loop:true,
+    margin:0,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplaySpeed: 800,  
+    autoplayHoverPause:true
+  
+      
+      
+      
+      
+      
+      
+      
+      
+      
+  });
+});
